6f4c1829ee659c5bf0231628d9af4aa2adf0be86
proactive-analyst-6157 <u.batsaikhan@gmail.com>
2020-05-27 16:30:09 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,5 +1,9 @@
 class Application
   def call
-    
+    if Time.now.hour.to_i >= 12
+      resp.write "Good Afternoon!"
+    elsif Time.now.hour.to_i < 12
+      resp.write "Good Morning!"
+    end
   end
 end
